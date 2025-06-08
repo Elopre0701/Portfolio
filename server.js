a49c90bb7b5c80e0a3ea5930e5bf39f53286b32c
Jhon Elopre <143986904+Elopre0701@users.noreply.github.com>
2025-06-08 13:39:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware to add CORS headers to all responses
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.header('Access-Control-Allow-Origin', 'https://elopre0701.github.io');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   next();
